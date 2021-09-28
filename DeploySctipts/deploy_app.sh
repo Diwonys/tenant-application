bf5c4e453cccc7221b9a457152d08dc8cd812b5f
Diwonys <Diwonusojo71828@gmail.com>
2021-09-28 15:10:28 +0300

    change deploy

--- DeploySctipts/deploy_app.sh
+++ DeploySctipts/deploy_app.sh
@@ -3,9 +3,6 @@ docker stop $(docker ps -qa)
 docker rm $(docker ps -qa)
 docker rmi $(docker images -q)
 docker pull $1
-#docker run $1
-docker-compose -f "/bin/docker_config_bash/docker-compose.yml" -f "/bin/docker_config_bash/docker-compose.override.yml" up
-#docker-compose.vs.release.g.yml
 
-# 1>docker-compose  -f "D:\Intership\tenant-application\WebApplication\docker-compose.yml" -f "D:\Intership\tenant-application\WebApplication\docker-compose.override.yml" -p dockercompose17129638450449943462 --no-ansi build
-# docker-compose  -f "D:\Intership\tenant-application\WebApplication\docker-compose.yml" -f "D:\Intership\tenant-application\WebApplication\docker-compose.override.yml" -f "D:\Intership\tenant-application\WebApplication\obj\Docker\docker-compose.vs.release.g.yml" -p dockercompose17129638450449943462 --no-ansi up -d --no-build
+docker-compose -f "/bin/docker_config_bash/docker-compose.yml" -f "/bin/docker_config_bash/docker-compose.override.yml" up
+#docker-compose -f "/bin/docker_config_bash/docker-compose.yml" -f "/bin/docker_config_bash/docker-compose.vs.release.g.yml" up
