bcd6bb4fcd89586556e7147d88d2a5e9ebeda166
Diwonys <Diwonusojo71828@gmail.com>
2021-09-30 15:57:35 +0300

    change bash sequense

--- DeploySctipts/deploy_app.sh
+++ DeploySctipts/deploy_app.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cd tenant-application
-git pull
 docker stop $(docker ps -qa)
 docker rm $(docker ps -qa)
 docker rmi $(docker images -q)
+
+cd tenant-application
+git pull
 
 cd WebApplication
 docker-compose -f "docker-compose.yml" -f "docker-compose.prod.yml" up -d
