83bbf15855617b489cb1fa728fde1219dc9e5943
Diwonys <Diwonusojo71828@gmail.com>
2021-09-30 16:05:22 +0300

    test bash

--- DeploySctipts/deploy_app.sh
+++ DeploySctipts/deploy_app.sh
@@ -4,8 +4,10 @@
 # docker rm $(docker ps -qa)
 # docker rmi $(docker images -q)
 
-cd tenant-application
-git pull
+# cd tenant-application
+# git pull
 
 # cd WebApplication
 # docker-compose -f "docker-compose.yml" -f "docker-compose.prod.yml" up -d
+
+touch test.txt
