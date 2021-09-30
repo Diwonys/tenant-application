b2758659cc4ce7b3b15b343bd0d08adfd920737e
Diwonys <Diwonusojo71828@gmail.com>
2021-09-30 16:02:30 +0300

    check deploy file git

--- DeploySctipts/deploy_app.sh
+++ DeploySctipts/deploy_app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker stop $(docker ps -qa)
-docker rm $(docker ps -qa)
-docker rmi $(docker images -q)
+# docker stop $(docker ps -qa)
+# docker rm $(docker ps -qa)
+# docker rmi $(docker images -q)
 
 cd tenant-application
 git pull
 
-cd WebApplication
-docker-compose -f "docker-compose.yml" -f "docker-compose.prod.yml" up -d
+# cd WebApplication
+# docker-compose -f "docker-compose.yml" -f "docker-compose.prod.yml" up -d
